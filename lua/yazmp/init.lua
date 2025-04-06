ab4286bb418d5026bb4a81dfa0af826485e647a3
itztla <hanumanman37@gmail.com>
2025-04-07 00:25:34 +0700

    feat: make buffers readonly & auto close

--- lua/yazmp/init.lua
+++ lua/yazmp/init.lua
@@ -11,24 +11,39 @@ local function create_window(width, direction)
 	vim.o.numberwidth = 1
 	vim.wo.rnu = false
 	vim.wo.nu = false
+	-- Make buffer readonly
+	vim.bo.readonly = true
+	vim.bo.modifiable = false
 end
 
 function M.zenmode(c)
 	if M.buf == nil then
 		M.buf = vim.api.nvim_create_buf(false, false)
-
 		local width = 30
 		if #c.fargs == 1 then
 			width = tonumber(c.fargs[1])
 		end
-
 		local cur_win = vim.fn.win_getid()
-
 		create_window(width, "H")
 		create_window(width, "L")
-
 		vim.api.nvim_set_current_win(cur_win)
+
+		-- Set up autocmd to close buffers on exit
+		if not M.exit_autocmd_id then
+			M.exit_autocmd_id = vim.api.nvim_create_autocmd("VimLeavePre", {
+				callback = function()
+					if M.buf and vim.api.nvim_buf_is_valid(M.buf) then
+						vim.api.nvim_buf_delete(M.buf, { force = true })
+						M.buf = nil
+					end
+				end,
+			})
+		end
 	else
+		if M.exit_autocmd_id then
+			vim.api.nvim_del_autocmd(M.exit_autocmd_id)
+			M.exit_autocmd_id = nil
+		end
 		vim.api.nvim_buf_delete(M.buf, { force = true })
 		M.buf = nil
 	end
